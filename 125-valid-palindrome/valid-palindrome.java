e320de393a16ce35204640267aff08ad4b273d1a
Arpan Sankesh <121539675+ArpanSankesh@users.noreply.github.com>
2025-06-07 15:43:16 +0530

    Time: 15 ms (21.24%) | Memory: 44.6 MB (44.93%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String result = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int start = 0,
+            end = result.length() - 1;
+
+            while(start <= end){
+                if(result.charAt(start) != result.charAt(end)){
+                return false;
+                }
+                    start++;
+                    end--;
+            }
+            return true;
+    }
+}
