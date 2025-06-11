d15becd82eafdb8c4a3e24ca60d9cadae10b66e6
Arpan Sankesh <121539675+ArpanSankesh@users.noreply.github.com>
2025-06-11 12:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (64.00%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        
-        for(int i = 0; i <= 30; i++){
-            double pow = Math.pow(2,i);
-            if (pow == n) return true;
-            if (pow > n ) break;
-        }
-        return false;
+        if( n <=0) return false;
+        if(n == 1) return true;
+        if( n% 2 !=0 ) return false;
+        return isPowerOfTwo(n / 2);
+
     }
 }
