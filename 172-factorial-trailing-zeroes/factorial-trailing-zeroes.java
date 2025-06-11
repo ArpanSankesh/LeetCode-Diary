ce9655265af3426c0b8a1b015dd698163a6a35be
Arpan Sankesh <121539675+ArpanSankesh@users.noreply.github.com>
2025-06-11 17:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (25.14%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int result = 0;
+        for(int i = 5; i<=n; i*=5){
+            result+= n/i;
+        }
+        return result;
+    }
+}
