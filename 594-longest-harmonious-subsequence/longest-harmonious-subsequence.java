b2f39bf68d1bd856337efb3b9f7dbf9590da3044
Arpan Sankesh <121539675+ArpanSankesh@users.noreply.github.com>
2025-06-30 17:07:04 +0530

    Time: 16 ms (62.12%) | Memory: 45.8 MB (48.60%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -3,17 +3,14 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int longestHarmSeq = 0;
         int n = nums.length;
-        for(int i = 0; i < n; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) +1);            
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) +1);            
         }
-        for(int i = 0; i < n; i++){
-            int min = nums[i];
-            int max = min + 1;
-            int count = 0;
-            if(map.containsKey(min) && map.containsKey(max)){
-                count = map.get(min) + map.get(max);
-            }
+        for(int key : map.keySet()){
+            if(map.containsKey(key + 1)){
+            int count = map.get(key) + map.get(key + 1);
             longestHarmSeq = Math.max(longestHarmSeq, count);
+            }
         }
         return longestHarmSeq;
     }
