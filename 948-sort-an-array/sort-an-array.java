0ff0b49317f2f9b56abe26a75dc11c864a3f07e0
Arpan Sankesh <121539675+ArpanSankesh@users.noreply.github.com>
2025-07-03 13:11:42 +0530

    Time: 47 ms (27.25%) | Memory: 55.7 MB (78.66%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,26 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+
+        
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int minE = nums[0], maxE = nums[0];
+        for(int num : nums){
+            if (num < minE) minE = num;
+            if (num > maxE) maxE = num;
+        }
+        int i = 0;
+        for(int num = minE; num <= maxE; num++){
+            while (map.getOrDefault(num, 0) > 0){
+                nums[i++] = num;
+                map.put(num, map.get(num) - 1);
+            } 
+        }
         return nums;
     }
 }
+
+                
